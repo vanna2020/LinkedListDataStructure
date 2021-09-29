1c9576ad410c0b42510ef4aa79166065cf1ebc25
vanna2020 <vannasingh1999@gmail.com>
2021-09-29 01:04:28 -0700

    welcome to data structure

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data structure problems using Generic");
+
+    }
+}
