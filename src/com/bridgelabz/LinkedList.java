25f84350e64ce5235ba07f47b56277a139ef8862
vanna2020 <vannasingh1999@gmail.com>
2021-09-29 01:20:33 -0700

    Linked List for Adding Three Number

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,9 +1,23 @@
 package com.bridgelabz;
 
 public class LinkedList {
-
+    public static void display(){
+        System.out.println("Welcome to Data Structure Problems using Java Generics");
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Data structure problems using Generic");
+        display();
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
 
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
